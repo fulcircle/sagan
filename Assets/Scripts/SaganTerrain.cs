04479215d7b13f05f55a959a53851b14a521ddb0
Vikram Sunkavalli <vsunkavalli@gmail.com>
2016-02-18 20:52:51 -0500

    WIP

---
+++ Assets/Scripts/SaganTerrain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SaganTerrain : MonoBehaviour {
+
+    public int terrainSize = 10;
+
+    protected List<QuadMesh> quads = new List<QuadMesh>();
+
+    protected QuadMesh rootQuad = GetComponent<GameObject>().AddComponent<QuadMesh>();
+
+    void Start() {
+        transform.position = new Vector3(0,0,0);
+
+        rootQuad.width = 10;
+        rootQuad.LOD = 2;
+    }
+}
